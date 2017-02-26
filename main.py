29ac0dce089256eb15e3570898ffb2ea79e9cef6
Kerlin Michel <michelkerlin@gmail.com>
2017-02-26 04:34:42 -0500

    Added socket functionality

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import serial
+import socket
+import sys
 
 #Add port
-ser = serial.Serial('', 9600)
+#ser = serial.Serial('', 9600)
 
-#Test input
-ser.write(b'5')
+#Test write
+#ser.write(b'5')
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_address = ('kerlin.tech', 5001)
+
+sock.connect(server_address)
+
+try:
+  while True:
+    data = sock.recv(1024)
+    print(data)
+    #ser.write(data)
+
+finally:
+  print('Closing client')
+  sock.close()
